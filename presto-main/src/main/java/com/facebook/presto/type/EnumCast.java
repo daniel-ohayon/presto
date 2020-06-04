40d17413d31f5c996802b948b077ecffb11f553b
Daniel Ohayon <dohayon@fb.com>
2020-06-04 16:44:13 -0700

    add other cast function

--- presto-main/src/main/java/com/facebook/presto/type/EnumCast.java
+++ presto-main/src/main/java/com/facebook/presto/type/EnumCast.java
@@ -6,6 +6,8 @@ import com.facebook.presto.spi.function.ScalarOperator;
 import com.facebook.presto.spi.function.SqlType;
 import io.airlift.slice.Slice;
 
+import java.util.Optional;
+
 import static com.facebook.presto.common.function.OperatorType.CAST;
 import static com.facebook.presto.common.type.NumericEnumType.MOOD_ENUM;
 import static com.facebook.presto.spi.StandardErrorCode.INVALID_CAST_ARGUMENT;
@@ -23,5 +25,15 @@ public final class EnumCast
             throw new PrestoException(INVALID_CAST_ARGUMENT, "Invalid enum value");
         }
         return numericValue;
+    }
+
+    @ScalarOperator(CAST)
+    @SqlType("Mood")
+    public static long castToMood(@SqlType(StandardTypes.INTEGER) long value)
+    {
+        if (!MOOD_ENUM.entries.values().contains(value)) {
+            throw new PrestoException(INVALID_CAST_ARGUMENT, "Invalid enum value");
+        }
+        return value;
     }
 }
